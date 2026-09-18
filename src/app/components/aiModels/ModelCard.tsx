7ec6fe1954166fa98161f37530f87bdeeaa4b145
sempuko <yulia.pavlova@viacesi.fr>
2026-09-18 12:02:46 +0200

    dans le studio

--- src/app/components/aiModels/ModelCard.tsx
+++ src/app/components/aiModels/ModelCard.tsx
@@ -85,7 +85,7 @@ export function ModelCard({ model, onOpenDetail, dense = false }: { model: AiMod
               Essayer dans le studio
             </ShimBtn>
           ) : (
-            <VBtn sm full disabled>bientôt disponible!</VBtn>
+            <VBtn sm full disabled>Bientôt disponible dans le Studio!</VBtn>
           )}
         </div>
       </div>
